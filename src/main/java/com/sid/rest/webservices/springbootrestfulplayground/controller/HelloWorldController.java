deadccafe5652c28ecbb5097af4cfd4b2508d716
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2017-11-18 08:45:28 -0800

    exposing service having Path Variable

--- src/main/java/com/sid/rest/webservices/springbootrestfulplayground/controller/HelloWorldController.java
+++ src/main/java/com/sid/rest/webservices/springbootrestfulplayground/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.sid.rest.webservices.springbootrestfulplayground.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sid.rest.webservices.springbootrestfulplayground.pojo.HelloWorldbean;
@@ -16,6 +17,11 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldbean helloWorldBean() {
 		return new HelloWorldbean("Hello World");
+	}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldbean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldbean(String.format("Hello World,%s",name));
 	}
 	
 
