c0af52d90052ab2b849c638c373cd230d8010cb7
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2017-11-18 23:05:12 -0800

    user service creation

---
+++ src/main/java/com/sid/rest/webservices/springbootrestfulplayground/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.sid.rest.webservices.springbootrestfulplayground.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sid.rest.webservices.springbootrestfulplayground.pojo.User;
+import com.sid.rest.webservices.springbootrestfulplayground.service.UserDaoService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserDaoService service;
+
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user){
+		User savedUser = service.save(user);
+	}
+}
