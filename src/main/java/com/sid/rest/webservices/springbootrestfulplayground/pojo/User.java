bed8a109c5f2c15836ba25f3f43b40dbcf2d883a
sudhanshu shekhar <sudhanshu.shekharr@gmail.com>
2017-11-24 10:11:45 -0800

    Swagger Documentation

--- src/main/java/com/sid/rest/webservices/springbootrestfulplayground/pojo/User.java
+++ src/main/java/com/sid/rest/webservices/springbootrestfulplayground/pojo/User.java
@@ -6,14 +6,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+@ApiModel("User model defination")
 public class User {
-
+	
+	
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast two character")
+	@ApiModelProperty(notes="minimum 2 charater required")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
 	
 	protected User() {
